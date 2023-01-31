04fbdb9ccefbe03ae394d38a9635cab3821cc594
yongtaeklee <dragontaek98@naver.com>
2023-02-01 06:49:50 +0900

    feat: Add BOJ_2559.java

---
+++ yongtaek/BOJ_2559.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.StringBuilder;
+import java.util.StringTokenizer;
+
+public class BOJ_2559 { 
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] array = new int[n];
+        
+        st = new StringTokenizer(br.readLine()," ");
+        
+        for(int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0 ; i < n - k + 1; i++) {
+            int sum = 0;
+			for (int j = i; j < i + k; j++) {
+				sum += array[j];
+			}
+			
+			max = Math.max(max, sum);
+        }
+        
+        System.out.println(max);
+    }
+}
