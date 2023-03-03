452f9e0a176953e93ae8d015dc25e4b01a85a71f
Junwoo Jung <39089969+ler0n@users.noreply.github.com>
2023-03-03 17:02:45 +0900

    feat: Add PROGRAMMERS_블록 이동하기.cpp

---
+++ junwoo/PROGRAMMERS_블록 이동하기.cpp
@@ -0,0 +1,143 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+struct state{
+    int y, x, dir, cnt;
+};
+
+int solution(vector<vector<int>> board) {
+    int answer;
+    queue<state> que;
+    bool visitied[2][101][101];
+    
+    memset(visitied, 0, sizeof(visitied));
+    que.push({0, 1, 0, 0});
+    visitied[0][0][1] = true;
+    while(!que.empty()){
+        int y, x;
+        state cur = que.front();
+        
+        que.pop();
+        if(cur.y == (board.size() - 1) && cur.x == (board[0].size() - 1)){
+            answer = cur.cnt;
+            break;
+        }
+        if(cur.dir == 0){
+            // 회전 상단
+            y = cur.y;
+            x = cur.x;
+            if(y > 0 && board[y - 1][x - 1] != 1 && board[y - 1][x] != 1){
+                if(!visitied[1][y][x - 1]){
+                    visitied[1][y][x - 1]  = true;
+                    que.push({y, x - 1, 1, cur.cnt + 1});
+                }
+                if(!visitied[1][y][x]){
+                    visitied[1][y][x] = true;
+                    que.push({y, x, 1, cur.cnt + 1});
+                }
+            }
+            // 회전 하단
+            y = cur.y + 1;
+            x = cur.x;
+            if(y < board.size() && board[y][x - 1] != 1 && board[y][x] != 1){
+                if(!visitied[1][y][x - 1]){
+                    visitied[1][y][x - 1] = true;
+                    que.push({y, x - 1, 1, cur.cnt + 1});
+                }
+                if(!visitied[1][y][x]){
+                    visitied[1][y][x] = true;
+                    que.push({y, x, 1, cur.cnt + 1});
+                }
+            }
+            // 이동 상
+            y = cur.y - 1;
+            x = cur.x;
+            if(y > -1 && board[y][x - 1] != 1 && board[y][x] != 1 && !visitied[0][y][x]){
+                visitied[0][y][x] = true;
+                que.push({y, x, 0, cur.cnt + 1});
+            }
+            // 이동 하
+            y = cur.y + 1;
+            x = cur.x;
+            if(y < board.size() && board[y][x - 1] != 1 && board[y][x] != 1 && !visitied[0][y][x]){
+                visitied[0][y][x] = true;
+                que.push({y, x, 0, cur.cnt + 1});
+            }
+            // 이동 좌
+            y = cur.y;
+            x = cur.x - 1;
+            if(x > 0 && board[y][x - 1] != 1 && !visitied[0][y][x]){
+                visitied[0][y][x] = true;
+                que.push({y, x, 0, cur.cnt + 1});
+            }
+            // 이동 우
+            y = cur.y;
+            x = cur.x + 1;
+            if(x < board[0].size() && board[y][x] != 1 && !visitied[0][y][x]){
+                visitied[0][y][x] = true;
+                que.push({y, x, 0, cur.cnt + 1});
+            }
+        }
+        else{
+            // 회전 좌측
+            y = cur.y;
+            x = cur.x;
+            if(x > 0 && board[y - 1][x - 1] != 1 && board[y][x - 1] != 1){
+                if(!visitied[0][y - 1][x]){
+                    visitied[0][y - 1][x] = true;
+                    que.push({y - 1, x, 0, cur.cnt + 1});
+                }
+                if(!visitied[0][y][x]){
+                    visitied[0][y][x] = true;
+                    que.push({y, x, 0, cur.cnt + 1});
+                }
+            }
+            // 회전 우측
+            y = cur.y;
+            x = cur.x + 1;
+            if(x < board[0].size() && board[y - 1][x] != 1 && board[y][x] != 1){
+                if(!visitied[0][y - 1][x]){
+                    visitied[0][y - 1][x] = true;
+                    que.push({y - 1, x, 0, cur.cnt + 1});
+                }
+                if(!visitied[0][y][x]){
+                    visitied[0][y][x] = true;
+                    que.push({y, x, 0, cur.cnt + 1});
+                }
+            }
+            // 이동 상측
+            y = cur.y - 1;
+            x = cur.x;
+            if(y > 0 && board[y - 1][x] != 1 && !visitied[1][y][x]){
+                visitied[1][y][x] = true;
+                que.push({y, x, 1, cur.cnt + 1});
+            }
+            // 이동 하측
+            y = cur.y + 1;
+            x = cur.x;
+            if(y < board.size() && board[y][x] != 1 && !visitied[1][y][x]){
+                visitied[1][y][x] = true;
+                que.push({y, x, 1, cur.cnt + 1});
+            }
+            // 이동 좌측
+            y = cur.y;
+            x = cur.x - 1;
+            if(x > -1 && board[y - 1][x] != 1 && board[y][x] != 1 && !visitied[1][y][x]){
+                visitied[1][y][x] = true;
+                que.push({y, x, 1, cur.cnt + 1});
+            }
+            // 이동 우측
+            y = cur.y;
+            x = cur.x + 1;
+            if(x < board[0].size() && board[y - 1][x] != 1 && board[y][x] != 1 && !visitied[1][y][x]){
+                visitied[1][y][x] = true;
+                que.push({y, x, 1, cur.cnt + 1});
+            }
+        }
+    }
+    return answer;
+}
