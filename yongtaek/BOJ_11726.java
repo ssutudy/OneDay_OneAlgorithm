f450809732b84767154a43cd92e6234a2d9a494c
dragontaek-lee <dragontaek98@naver.com>
2023-05-12 01:56:28 +0900

    feat: Add BOJ_11726.java

---
+++ yongtaek/BOJ_11726.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class BOJ_11726 {
+    static int[] dp = new int[1001];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
